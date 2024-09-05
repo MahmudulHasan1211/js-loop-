9499a5ee044966b3b4477638dbb8b8e5c68e462a
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-09-05 14:22:02 +0600

    Even / odd numbers find added

--- Loop.js
+++ Loop.js
@@ -1,32 +1,54 @@
-// for of 
-const names = ['Rock', 'Dens', 'Alice', 'Bob'];
-console.log(names)
-for(const name of names){
-    console.log(name)
-    console.log('He is my friend')
-}
+// // for of 
+// const names = ['Rock', 'Dens', 'Alice', 'Bob'];
+// console.log(names)
+// for(const name of names){
+//     console.log(name)
+//     console.log('He is my friend')
+// }
 
 
-// while loop 
-let num1 = 1;  //loop variable
-while (num1 < 5){
-    console.log('My name is mahmudul');
-    num1 = num1 + 1 ;
-}
+// // while loop 
+// let num1 = 1;  //loop variable
+// while (num1 < 5){
+//     console.log('My name is mahmudul');
+//     num1 = num1 + 1 ;
+// }
 
-// reverse print 
-let neg = 100;
-while (neg > 0){
-    console.log(neg)
-    neg--;
-}
+// // reverse print 
+// let neg = 100;
+// while (neg > 0){
+//     console.log(neg)
+//     neg--;
+// }
 
-// summation of numbers 
-let  num = 1;
-sum = 0;
-while(num < 11 ){
-    console.log(num);
-    sum = sum + num;
-    console.log('sum:', sum)
-    num++;
+// // summation of numbers 
+// let  num = 1;
+// sum = 0;
+// while(num < 11 ){
+//     console.log(num);
+//     sum = sum + num;
+//     console.log('sum:', sum)
+//     num++;
+// }
+
+
+// // print even numbers 
+// let num = 1 ;
+// while( num <= 10){
+//     console.log(num)
+//     if (num % 2 == 0){
+//         console.log('even numbers:', num)
+
+//     }
+//     num++;
+// }
+// print odd numbers 
+let Enum = 1 ;
+while( Enum <= 10){
+    console.log(Enum)
+    if (Enum % 2 == 1){
+        console.log('odd numbers:', Enum)
+
+    }
+    Enum++;
 }
