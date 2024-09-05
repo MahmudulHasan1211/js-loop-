6f23e0f23033ccdfaca9bc01a15f9917e24ea99a
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-09-05 14:04:25 +0600

    first commit

---
+++ Loop.js
@@ -0,0 +1,22 @@
+// for of 
+const names = ['Rock', 'Dens', 'Alice', 'Bob'];
+console.log(names)
+for(const name of names){
+    console.log(name)
+    console.log('He is my friend')
+}
+
+
+// while loop 
+let num1 = 1;  //loop variable
+while (num1 < 5){
+    console.log('My name is mahmudul');
+    num1 = num1 + 1 ;
+}
+
+// reverse print 
+let neg = 100;
+while (neg > 0){
+    console.log(neg)
+    neg--;
+}
