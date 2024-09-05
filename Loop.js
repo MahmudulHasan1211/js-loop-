79ff2309b2289a63a8f805da6a1a4810b55e35d8
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-09-05 14:14:08 +0600

    summation of numbers added

--- Loop.js
+++ Loop.js
@@ -20,3 +20,13 @@ while (neg > 0){
     console.log(neg)
     neg--;
 }
+
+// summation of numbers 
+let  num = 1;
+sum = 0;
+while(num < 11 ){
+    console.log(num);
+    sum = sum + num;
+    console.log('sum:', sum)
+    num++;
+}
